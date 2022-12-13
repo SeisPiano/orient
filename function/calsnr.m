fa9ae80a3c1d646261c5c023e95cb399718c8b19
SeisPiano <12131066@mail.sustech.edu.cn>
2022-12-13 11:14:52 +0800

    add another snr method

--- function/calsnr.m
+++ function/calsnr.m
@@ -1,28 +1,82 @@
-function snr = calsnr(data,otime,btime,dist,dt)
+function snr = calsnr(data,otime,btime,dist,dt,snr_op)
 % original version was written by Dr. Ba Manh Le.
 % the snr is calculated by sqrt(∑signal^2/∑noise^2)
-rayl_vel = [3 4]; % lower and upper limits of Rayleigh wave velocity
-nois_vel = [2 2.5]; % lower and upper limits of noise velocity
-sigwin = round((dist./fliplr(rayl_vel)+otime-btime)/dt)+1;
-noiwin = round((dist./fliplr(nois_vel)+otime-btime)/dt)+1;
-if sigwin(1)<1
-    sigwin(1)=1;
-end
-if noiwin(2)>length(data)
-    noiwin(2)=length(data);
-end
-if noiwin(1)>=length(data) || sigwin(2)<1
-    snr = 0;
-else
-    lensigwin = sigwin(2)-sigwin(1);
-    lennoiwin = noiwin(2)-noiwin(1);
 
-    if lensigwin > lennoiwin
+if nargin == 5
+    snr_op = 1;
+end
+
+if snr_op == 1
+
+    rayl_vel = [3 4]; % lower and upper limits of Rayleigh wave velocity
+    nois_vel = [2 2.5]; % lower and upper limits of noise velocity
+    sigwin = round((dist./fliplr(rayl_vel)+otime-btime)/dt)+1;
+    noiwin = round((dist./fliplr(nois_vel)+otime-btime)/dt)+1;
+    if sigwin(1)<1
+        sigwin(1)=1;
+    end
+    if noiwin(2)>length(data)
+        noiwin(2)=length(data);
+    end
+    if noiwin(1)>=length(data) || sigwin(2)<1
         snr = 0;
     else
-        snr = sum(data(sigwin(1):sigwin(2)).^2)/sum(data(noiwin(1):noiwin(2)).^2);
-        snr = sqrt(snr);
+        lensigwin = sigwin(2)-sigwin(1);
+        lennoiwin = noiwin(2)-noiwin(1);
+
+        if lensigwin > lennoiwin
+            snr = 0;
+        else
+            snr = sum(data(sigwin(1):sigwin(2)).^2)/sum(data(noiwin(1):noiwin(2)).^2);
+            snr = sqrt(snr);
+        end
     end
+
+elseif snr_op == 2
+
+    % This function refers to the noise cross-correlation method of Huajian Yao
+    % Reference: http://yaolab.ustc.edu.cn/resources.php?i=28
+    % the snr is calculated by max(abs(signal)/mean(abs(noise))
+    %
+    % 2022-10-25
+    % Yuechu Wu
+    % 12131066@mail.sustech.edu.cn
+
+    velovity = [0.5, 5]; % lower and upper limits of Rayleigh wave velocity
+    noise_win_len = 150; % length of noise window (s), which is defined right after the signal window
+
+    signal_win = round((dist./fliplr(velovity)+otime-btime)/dt)+1;
+    noise_win  = [signal_win(2), signal_win(2) + round(noise_win_len/dt)];
+
+    if signal_win(1) < 1
+        signal_win(1) = 1;
+    end
+
+    if noise_win(2) > length(data)
+        noise_win(2) = length(data);
+    end
+
+    if noise_win(1) >= length(data)
+        snr = 0;
+    else
+        data_enve = abs(hilbert(data));
+        signal_amp_max = max(data_enve(signal_win));
+        noise_amp_mean = mean(data_enve(noise_win));
+
+        if noise_amp_mean == 0
+            if signal_amp_max > 0
+                snr = 100;
+            else
+                snr = 0;
+            end
+        else
+            snr = signal_amp_max/noise_amp_mean; % max signal envelope amplitue / average noise envelope amplitude
+        end
+
+    end
+
+else
+    error('Incorrect snr_op! Expected value is 1 or 2.');
 end
 
 return
