c25993a51a668358a1d5396ed69150c5e7960197
SeisPiano <12131066@mail.sustech.edu.cn>
2024-05-02 17:44:15 +0800

    update

--- function/calsnr.m
+++ function/calsnr.m
@@ -12,24 +12,17 @@ if snr_op == 1
     nois_vel = [2 2.5]; % lower and upper limits of noise velocity
     sigwin = round((dist./fliplr(rayl_vel)+otime-btime)/dt)+1;
     noiwin = round((dist./fliplr(nois_vel)+otime-btime)/dt)+1;
-    if sigwin(1)<1
-        sigwin(1)=1;
+
+    if noiwin(2) > length(data)
+        noiwin(2) = length(data);
     end
-    if noiwin(2)>length(data)
-        noiwin(2)=length(data);
-    end
-    if noiwin(1)>=length(data) || sigwin(2)<1
+    if noiwin(1) >= length(data)
         snr = 0;
     else
-        lensigwin = sigwin(2)-sigwin(1);
-        lennoiwin = noiwin(2)-noiwin(1);
 
-        if lensigwin > lennoiwin
-            snr = 0;
-        else
-            snr = sum(data(sigwin(1):sigwin(2)).^2)/sum(data(noiwin(1):noiwin(2)).^2);
-            snr = sqrt(snr);
-        end
+        snr = sum(data(sigwin(1):sigwin(2)).^2)/sum(data(noiwin(1):noiwin(2)).^2);
+        snr = sqrt(snr);
+        
     end
 
 elseif snr_op == 2
